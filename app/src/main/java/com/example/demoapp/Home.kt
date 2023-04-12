e891b1633a380ae3d5a395b9b55fa0b78ccad0c5
jainil03 <100684304+jainil03@users.noreply.github.com>
2023-04-12 13:34:25 +0530

    Initial Commit

--- app/src/main/java/com/example/demoapp/Home.kt
+++ app/src/main/java/com/example/demoapp/Home.kt
@@ -1,59 +1,67 @@
 package com.example.demoapp
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
+@Suppress("DEPRECATION")
 class Home : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var deviceListAdapter: ArrayAdapter<String>
+    private val devices = mutableListOf<String>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        deviceListAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, devices)
+        view.findViewById<ListView>(R.id.deviceList).adapter = deviceListAdapter
+        view.findViewById<Button>(R.id.scanbtn).setOnClickListener {
+            listPairedDevices()
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        } else {
+            listPairedDevices()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    @SuppressLint("MissingPermission")
+    private fun listPairedDevices() {
+        devices.clear()
+        deviceListAdapter.notifyDataSetChanged()
+        val pairedDevices = bluetoothAdapter.bondedDevices
+        if (pairedDevices.isNotEmpty()) {
+            for (device in pairedDevices) {
+                devices.add("${device.name} (${device.address})")
+            }
+            deviceListAdapter.notifyDataSetChanged()
+        } else {
+            Toast.makeText(requireContext(), "No paired devices found", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Home().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val REQUEST_ENABLE_BT = 1
     }
 }
