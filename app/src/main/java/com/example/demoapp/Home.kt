8460b49a47d6571f7d9308bdfb1a59728e143676
jainil03 <100684304+jainil03@users.noreply.github.com>
2023-05-07 15:23:56 +0530

    Initial Commit

--- app/src/main/java/com/example/demoapp/Home.kt
+++ app/src/main/java/com/example/demoapp/Home.kt
@@ -2,25 +2,34 @@ package com.example.demoapp
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import java.io.IOException
+import java.util.*
 
 @Suppress("DEPRECATION")
 class Home : Fragment() {
-    // TODO: Rename and change types of parameters
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private lateinit var deviceListAdapter: ArrayAdapter<String>
     private val devices = mutableListOf<String>()
+    private lateinit var socket: BluetoothSocket
+    private lateinit var device: BluetoothDevice
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,7 +39,7 @@ class Home : Fragment() {
         deviceListAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, devices)
         view.findViewById<ListView>(R.id.deviceList).adapter = deviceListAdapter
         view.findViewById<Button>(R.id.scanbtn).setOnClickListener {
-            listPairedDevices()
+            scanForDevices()
         }
         return view
     }
@@ -44,7 +53,13 @@ class Home : Fragment() {
         } else {
             listPairedDevices()
         }
+
+        view.findViewById<ListView>(R.id.deviceList).onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                connectToDevice(position)
+            }
     }
+
 
     @SuppressLint("MissingPermission")
     private fun listPairedDevices() {
@@ -61,7 +76,65 @@ class Home : Fragment() {
         }
     }
 
+    @SuppressLint("MissingPermission")
+    private fun scanForDevices() {
+        devices.clear()
+        deviceListAdapter.notifyDataSetChanged()
+
+        if (!bluetoothAdapter.startDiscovery()) {
+            Toast.makeText(requireContext(), "Discovery failed to start.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        requireActivity().registerReceiver(receiver, filter)
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        @SuppressLint("MissingPermission")
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action = intent?.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                if (device != null && !devices.contains(
+                        "${device.name} (${device.address})"
+                )) {
+                    devices.add("${device.name} (${device.address})")
+                    deviceListAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun connectToDevice(position: Int) {
+        val deviceString = deviceListAdapter.getItem(position)
+        val deviceAddress = deviceString?.substring(deviceString.indexOf("(") + 1, deviceString.length - 1)
+        deviceAddress?.let {
+            device = bluetoothAdapter.getRemoteDevice(it)
+            try {
+                socket = device.createRfcommSocketToServiceRecord(MY_UUID)
+                socket.connect()
+                Toast.makeText(requireContext(), "Device connected", Toast.LENGTH_SHORT).show()
+            } catch (e: IOException) {
+                Log.e(TAG, "Error connecting to device: ${e.message}")
+                Toast.makeText(requireContext(), "Error connecting to device", Toast.LENGTH_SHORT).show()
+            }
+        } ?: run {
+            Toast.makeText(requireContext(), "Invalid device address", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
     companion object {
         private const val REQUEST_ENABLE_BT = 1
+        private const val REQUEST_DISCOVERABLE_BT = 2
+        private val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        private const val TAG = "HomeFragment"
     }
+
+
+
 }
